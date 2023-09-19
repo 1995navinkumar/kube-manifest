74ab2ea24189851ad60adc7fa0821264202ee01b
1995navinkumar <navin.kumar@razorpay.com>
2023-09-19 21:11:39 +0530

    clusterIP

--- kubernetes/charts/nginx/nginx.tf
+++ kubernetes/charts/nginx/nginx.tf
@@ -1,5 +1,9 @@
 resource "helm_release" "nginx" {
-  name = "nginx"
+  name       = "nginx"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
+
+  values = [
+    "${file("values.yaml")}"
+  ]
 }
