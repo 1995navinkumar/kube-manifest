1afa5bbf215d29d509e48c95cb955378cc05dbad
1995navinkumar <navin.kumar@razorpay.com>
2023-09-23 14:42:01 +0530

    move provider to main

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,13 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host = var.kube-host
-
-    client_certificate     = var.client_certificate
-    client_key             = var.client_key
-    cluster_ca_certificate = var.cluster_ca_certificate
-  }
-}
-
 module "nginx" {
   source = "./charts/nginx"
 }
--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ terraform {
       version = "2.11.0"
     }
   }
+
+  kubernetes {
+    host = var.kube-host
+
+    client_certificate     = var.client_certificate
+    client_key             = var.client_key
+    cluster_ca_certificate = var.cluster_ca_certificate
+  }
 }
 
 module "kubernetes" {
