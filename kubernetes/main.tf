d51a7c36b3058de4e8eb695f788df72d1884524a
1995navinkumar <navin.kumar@razorpay.com>
2023-09-23 14:30:25 +0530

    use secret var

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,6 +1,10 @@
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host = var.kube-host
+
+    client_certificate     = var.client_certificate
+    client_key             = var.client_key
+    cluster_ca_certificate = var.cluster_ca_certificate
   }
 }
 
