820c25844f15584df24221b57563ec2e3492c9df
1995navinkumar <navin.kumar@razorpay.com>
2023-09-23 14:43:09 +0530

    lint

--- main.tf
+++ main.tf
@@ -11,15 +11,16 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.11.0"
     }
+
+    kubernetes {
+      host = var.kube-host
+
+      client_certificate     = var.client_certificate
+      client_key             = var.client_key
+      cluster_ca_certificate = var.cluster_ca_certificate
+    }
   }
 
-  kubernetes {
-    host = var.kube-host
-
-    client_certificate     = var.client_certificate
-    client_key             = var.client_key
-    cluster_ca_certificate = var.cluster_ca_certificate
-  }
 }
 
 module "kubernetes" {
