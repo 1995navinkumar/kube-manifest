aabf39c0205ba205940e63433fd30c5521c795c2
1995navinkumar <navin.kumar@razorpay.com>
2023-10-12 18:24:38 +0530

    decode

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -3,9 +3,9 @@ provider "helm" {
 
     host = var.kubehost
 
-    client_certificate     = var.client_certificate
-    client_key             = var.client_key
-    cluster_ca_certificate = var.cluster_ca_certificate
+    client_certificate     = base64decode(var.client_certificate)
+    client_key             = base64decode(var.client_key)
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   }
 }
 module "nginx" {
