07930d7add1c256f364be6b764141d7489624fec
1995navinkumar <navin.kumar@razorpay.com>
2023-10-12 20:01:42 +0530

    define

--- main.tf
+++ main.tf
@@ -17,5 +17,9 @@ terraform {
 }
 
 module "kubernetes" {
-  source = "./kubernetes"
+  source                 = "./kubernetes"
+  kubehost               = var.kubehost
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
 }
