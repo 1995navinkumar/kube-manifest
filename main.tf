2108734cd3059f345a65c560298007d673980375
1995navinkumar <navin.kumar@razorpay.com>
2023-10-12 20:10:24 +0530

    add var

--- main.tf
+++ main.tf
@@ -16,10 +16,18 @@ terraform {
 
 }
 
+variable "kubehost" {}
+
+variable "client_certificate" {}
+
+variable "client_key" {}
+
+variable "cluster_ca_certificate" {}
+
 module "kubernetes" {
   source                 = "./kubernetes"
-  kubehost               = "${var.kubehost}"
-  client_certificate     = "${var.client_certificate}"
-  client_key             = "${var.client_key}"
-  cluster_ca_certificate = "${var.cluster_ca_certificate}"
+  kubehost               = var.kubehost
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
 }
