d81f08fbf65fc5f21e59c79184f9d2f9689a378a
1995navinkumar <navin.kumar@razorpay.com>
2023-10-12 18:09:55 +0530

    define variables

---
+++ kubernetes/variables.tf
@@ -0,0 +1,15 @@
+variable "kubehost" {
+  type    = string
+}
+
+variable "client_certificate" {
+  type    = string
+}
+
+variable "client_key" {
+  type    = string
+}
+
+variable "cluster_ca_certificate" {
+  type    = string
+}
