df2c1c762b6184c85c3c30a9d2f34856e7b55447
1995navinkumar <navin.kumar@razorpay.com>
2023-10-22 10:28:08 +0530

    remove community

--- kubernetes/charts/prometheus/prometheus.tf
+++ kubernetes/charts/prometheus/prometheus.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-community/prometheus"
+  chart      = "prometheus"
 
   values = [
     "${file("${path.module}/values.yaml")}"
