a53b404668050cc59fec45eadd8f64a2ec4eadf8
1995navinkumar <navin.kumar@razorpay.com>
2023-10-22 16:59:32 +0530

    ingres

--- kubernetes/grafana/grafana.tf
+++ kubernetes/grafana/grafana.tf
@@ -1,3 +1,6 @@
+resource "kubernetes_manifest" "grafana-namespace" {
+  manifest = yamldecode(file("${path.module}/manifests/grafana-namespace.yaml"))
+}
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
@@ -6,4 +9,15 @@ resource "helm_release" "grafana" {
   values = [
     "${file("${path.module}/values.yaml")}"
   ]
+  depends_on = [kubernetes_manifest.grafana-namespace]
+}
+
+resource "kubernetes_manifest" "grafana-ingress-middleware" {
+  manifest   = yamldecode(file("${path.module}/manifests/grafana-ingress-middleware.yaml"))
+  depends_on = [helm_release.grafana]
+}
+
+resource "kubernetes_manifest" "grafana-ingress" {
+  manifest   = yamldecode(file("${path.module}/manifests/grafana-ingress.yaml"))
+  depends_on = [kubernetes_manifest.grafana-ingress-middleware]
 }
