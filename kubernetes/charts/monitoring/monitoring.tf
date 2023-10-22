f323ca3ec50fa534e4666eb09c1504f54b3f6368
1995navinkumar <navin.kumar@razorpay.com>
2023-10-22 12:13:55 +0530

    depends on

--- kubernetes/charts/monitoring/monitoring.tf
+++ kubernetes/charts/monitoring/monitoring.tf
@@ -1,19 +1,21 @@
+resource "kubernetes_manifest" "monitoring-namespace" {
+  manifest = yamldecode(file("${path.module}/manifests/monitoring-namespace.yaml"))
+}
 resource "helm_release" "prometheus" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
-  namespace        = "monitoring"
-  create_namespace = true
-
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
   values = [
     "${file("${path.module}/prometheus-values.yaml")}"
   ]
-}
-
-resource "kubernetes_manifest" "prometheus-ingress" {
-  manifest = yamldecode(file("${path.module}/manifests/prometheus-ingress.yaml"))
+  depends_on = [kubernetes_manifest.monitoring-namespace]
 }
 
 resource "kubernetes_manifest" "prometheus-ingress-middleware" {
-  manifest = yamldecode(file("${path.module}/manifests/prometheus-ingress-middleware.yaml"))
+  manifest   = yamldecode(file("${path.module}/manifests/prometheus-ingress-middleware.yaml"))
+  depends_on = [helm_release.prometheus]
+}
+resource "kubernetes_manifest" "prometheus-ingress" {
+  manifest   = yamldecode(file("${path.module}/manifests/prometheus-ingress.yaml"))
+  depends_on = [kubernetes_manifest.prometheus-ingress-middleware]
 }
