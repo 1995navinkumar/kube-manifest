b8eae86658369a6d78af5e8b8d4e9a728bfaafc4
1995navinkumar <navin.kumar@razorpay.com>
2023-10-23 09:32:37 +0530

    monitoring storage

--- kubernetes/storage-class/storage-class.tf
+++ kubernetes/storage-class/storage-class.tf
@@ -2,6 +2,6 @@ resource "kubernetes_manifest" "db-storage" {
   manifest = yamldecode(file("${path.module}/manifests/db-storage.yaml"))
 }
 
-resource "kubernetes_manifest" "log-storage" {
-  manifest = yamldecode(file("${path.module}/manifests/log-storage.yaml"))
+resource "kubernetes_manifest" "monitoring-storage" {
+  manifest = yamldecode(file("${path.module}/manifests/monitoring-storage.yaml"))
 }
