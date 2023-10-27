b1ad82530d2eaff83c9a3449f606cba86691986f
Navin kumar <navin.kumar@razorpay.com>
2023-10-27 06:10:17 +0530

    Local storage (#22)
    * configure default storage
    
    * delete other storage
    
    * resource

--- kubernetes/storage-class/storage-class.tf
+++ kubernetes/storage-class/storage-class.tf
@@ -1,7 +1,3 @@
-resource "kubernetes_manifest" "db-storage" {
-  manifest = yamldecode(file("${path.module}/manifests/db-storage.yaml"))
-}
-
-resource "kubernetes_manifest" "monitoring-storage" {
-  manifest = yamldecode(file("${path.module}/manifests/monitoring-storage.yaml"))
+resource "kubernetes_manifest" "local-storage-class" {
+  manifest = yamldecode(file("${path.module}/manifests/local-storage-class.yaml"))
 }
