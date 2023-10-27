0f0c7f888828e382ab7265157986580c06f2f0ee
1995navinkumar <navin.kumar@razorpay.com>
2023-10-27 06:58:42 +0530

    rename

--- kubernetes/storage/storage.tf
+++ kubernetes/storage/storage.tf
@@ -1,14 +1,14 @@
 resource "kubernetes_manifest" "local-storage-class" {
   manifest = yamldecode(file("${path.module}/manifests/local-storage-class.yaml"))
 }
-resource "kubernetes_manifest" "persistent-volume-3_1" {
+resource "kubernetes_manifest" "persistent-volume-3Gi_1" {
   manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
 }
 
-resource "kubernetes_manifest" "persistent-volume-3_2" {
+resource "kubernetes_manifest" "persistent-volume-3Gi_2" {
   manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
 }
 
-resource "kubernetes_manifest" "persistent-volume-3_3" {
+resource "kubernetes_manifest" "persistent-volume-3Gi_3" {
   manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
 }
