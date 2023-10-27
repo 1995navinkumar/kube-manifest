5293264bca01a2e0ecbe0f2ff81e53558c2bc869
1995navinkumar <navin.kumar@razorpay.com>
2023-10-27 06:56:51 +0530

    pv's

--- kubernetes/storage/storage.tf
+++ kubernetes/storage/storage.tf
@@ -2,5 +2,13 @@ resource "kubernetes_manifest" "local-storage-class" {
   manifest = yamldecode(file("${path.module}/manifests/local-storage-class.yaml"))
 }
 resource "kubernetes_manifest" "persistent-volume-3_1" {
-  manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3_1.yaml"))
+  manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
+}
+
+resource "kubernetes_manifest" "persistent-volume-3_2" {
+  manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
+}
+
+resource "kubernetes_manifest" "persistent-volume-3_3" {
+  manifest = yamldecode(file("${path.module}/manifests/persistent-volume-3Gi.yaml"))
 }
