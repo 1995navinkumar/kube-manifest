bc58fa3fc5ff066c216f3c4826a0c25fa2b60cb1
1995navinkumar <navin.kumar@razorpay.com>
2023-12-10 15:54:28 +0530

    pass variables

--- main.tf
+++ main.tf
@@ -22,4 +22,8 @@ module "kubernetes" {
   client_certificate     = var.client_certificate
   client_key             = var.client_key
   cluster_ca_certificate = var.cluster_ca_certificate
+
+  postgresAdminPassword = var.postgresAdminPassword
+  postgresUser          = var.postgresUser
+  postgresPassword      = var.postgresPassword
 }
