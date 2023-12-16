2d3025420ab688fd6b48ad2fa9d205628099f4ab
Navin kumar <navin.kumar@razorpay.com>
2023-12-16 16:21:38 +0530

    thirty inches schema (#30)
    * thirty inches schema
    
    * add job
    
    * rename resources
    
    * lint
    
    * update image
    
    * host
    
    * env var
    
    * disable metrics
    
    * disable cleanup
    
    * add plans table
    
    * version
    
    * uid
    
    * replace
    
    * scrape job logs
    
    * remove col
    
    * enable auto plan

--- kubernetes/postgresql/postgresql.tf
+++ kubernetes/postgresql/postgresql.tf
@@ -12,3 +12,83 @@ resource "helm_release" "postgresql" {
     })
   ]
 }
+
+resource "kubernetes_config_map" "sql_scripts" {
+  metadata {
+    name      = "sql-scripts"
+    namespace = "postgresql"
+  }
+
+  data = {
+    "thirtyinches_schema.sql" = "${file("${path.module}/scripts/thirtyinches/schema.sql")}"
+  }
+
+}
+
+resource "kubernetes_job" "run_sql_scripts" {
+  metadata {
+    name      = "run-sql-scripts"
+    namespace = "postgresql"
+  }
+
+  spec {
+    backoff_limit = 1
+    template {
+      metadata {
+        annotations = {
+          scrape_logs = "true"
+        }
+
+        labels = {
+          app = "run-sql-scripts-job"
+        }
+      }
+      spec {
+        restart_policy = "Never"
+
+        container {
+          name  = "postgres-client"
+          image = "postgres:alpine"
+
+          command = [
+            "psql",
+            "-h", "postgresql.postgresql",
+            "-d", "thirtyinches",
+            "-f", "/scripts/thirtyinches_schema.sql",
+          ]
+
+          env {
+            name  = "PGUSER"
+            value = var.postgresUser
+          }
+          env {
+            name  = "PGPASSWORD"
+            value = var.postgresPassword
+          }
+
+
+          volume_mount {
+            name       = "sql-scripts-volume"
+            mount_path = "/scripts"
+          }
+        }
+
+        volume {
+          name = "sql-scripts-volume"
+          config_map {
+            name = "sql-scripts"
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.postgresql, kubernetes_config_map.sql_scripts
+  ]
+  lifecycle {
+    replace_triggered_by = [
+      kubernetes_config_map.sql_scripts
+    ]
+  }
+}
---
+++ kubernetes/postgresql/scripts/thirtyinches/schema.sql
@@ -0,0 +1,45 @@
+-- Users table
+CREATE TABLE IF NOT EXISTS Users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Plans table
+CREATE TABLE IF NOT EXISTS Plans (
+    plan_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    trial_days INTEGER DEFAULT NULL
+);
+
+-- -- Subscriptions table
+-- CREATE TABLE IF NOT EXISTS Subscriptions (
+--     subscription_id SERIAL PRIMARY KEY,
+--     user_id INTEGER NOT NULL,
+--     plan_id INTEGER NOT NULL,
+--     start_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     end_date TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
+--     status ENUM('active', 'cancelled', 'expired') NOT NULL DEFAULT 'active',
+--     payment_method VARCHAR(255) DEFAULT NULL,
+--     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+--     FOREIGN KEY (plan_id) REFERENCES Plans(plan_id) ON DELETE RESTRICT
+-- );
+
+-- -- Transactions table
+-- CREATE TABLE IF NOT EXISTS Transactions (
+--     transaction_id SERIAL PRIMARY KEY,
+--     subscription_id INTEGER NOT NULL,
+--     amount DECIMAL(10, 2) NOT NULL,
+--     payment_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     payment_method VARCHAR(255) DEFAULT NULL,
+--     status ENUM('successful', 'failed', 'pending') NOT NULL DEFAULT 'pending',
+--     FOREIGN KEY (subscription_id) REFERENCES Subscriptions(subscription_id) ON DELETE CASCADE
+-- );
+
+-- -- Add any additional tables or constraints as needed
+-- -- Example of adding an index on email for faster lookup
+-- CREATE INDEX idx_users_email ON Users(email);
