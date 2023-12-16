d7815443d5887ccecce906557955ec8cd7faa644
1995navinkumar <navin.kumar@razorpay.com>
2023-12-16 09:57:14 +0530

    add job

--- kubernetes/postgresql/postgresql.tf
+++ kubernetes/postgresql/postgresql.tf
@@ -24,3 +24,56 @@ resource "kubernetes_config_map" "sql_scripts" {
   }
 
 }
+
+resource "kubernetes_job" "run_sql_scripts" {
+  metadata {
+    name      = "run_sql_scripts"
+    namespace = "postgresql"
+  }
+
+  spec {
+    template {
+      spec {
+        restart_policy = "Never"
+
+        container {
+          name  = "postgres-client"
+          image = "jbergknoff/postgresql-client"
+
+          command = [
+            "psql",
+            "-h", "localhost",
+            "-d", "thirtyinches",
+            "-f", "/scripts/thirtyinches_schema.sql",
+          ]
+
+          env {
+            name  = "POSTGRES_USER"
+            value = var.postgresUser
+          }
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = var.postgresPassword
+          }
+
+
+          volume_mounts {
+            name       = "sql_scripts_volume"
+            mount_path = "/scripts"
+          }
+        }
+
+        volume {
+          name = "sql_scripts_volume"
+          config_map {
+            name = "sql_scripts"
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.postgresql, kubernetes_config_map.sql_scripts
+  ]
+}
