a198c9fd358cd79da99d8a141581a54851dc6740
1995navinkumar <navin.kumar@razorpay.com>
2024-07-07 23:03:08 +0530

    middleware

--- kubernetes/minio/minio.tf
+++ kubernetes/minio/minio.tf
@@ -12,7 +12,12 @@ resource "helm_release" "minio" {
   depends_on = [kubernetes_manifest.minio-namespace]
 }
 
+resource "kubernetes_manifest" "minio-ingress-middleware" {
+  manifest   = yamldecode(file("${path.module}/manifests/minio-ingress-middleware.yaml"))
+  depends_on = [helm_release.minio]
+}
+
 resource "kubernetes_manifest" "minio-ingress" {
   manifest   = yamldecode(file("${path.module}/manifests/minio-ingress.yaml"))
-  depends_on = [helm_release.minio]
+  depends_on = [kubernetes_manifest.minio-ingress-middleware]
 }
