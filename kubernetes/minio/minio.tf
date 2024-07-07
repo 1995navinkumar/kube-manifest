f3bf7f7cfb11067e8e04defc22079d5446c41472
1995navinkumar <navin.kumar@razorpay.com>
2024-07-07 22:16:05 +0530

    add ns

--- kubernetes/minio/minio.tf
+++ kubernetes/minio/minio.tf
@@ -1,3 +1,6 @@
+resource "kubernetes_manifest" "minio-namespace" {
+  manifest = yamldecode(file("${path.module}/manifests/minio-namespace.yaml"))
+}
 resource "helm_release" "minio" {
   name       = "minio"
   repository = "oci://registry-1.docker.io/bitnamicharts"
@@ -6,4 +9,5 @@ resource "helm_release" "minio" {
   values = [
     "${file("${path.module}/values.yaml")}"
   ]
+  depends_on = [kubernetes_manifest.minio-namespace]
 }
